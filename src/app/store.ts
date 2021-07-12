2e3a6a847e0eb28537e3d6fc4d87e5706a4a5eb2
Claudio <claudio@DESKTOP-HA26NJA.localdomain>
2021-07-12 11:10:19 +0200

    removed npm

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 // ...
-const store = configureStore({
+
+export const store = configureStore({
     reducer: {
 
     },
 })
+
+
 export type RootState = ReturnType<typeof store.getState>
 
-export default store
+export type AppDispatch = typeof store.dispatch
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
